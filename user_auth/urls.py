c4ffd459b2a0a35764006025e8d629e9d650ad13
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-08-09 21:37:03 +0100

    Handling logout

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -8,9 +8,7 @@ from . import views
 app_name = 'user_auth'
 
 urlpatterns = [
-    path('', views.signin, name='signin'),
+    path('', auth_views.LoginView.as_view(template_name='user_auth/signin.html'), name='signin'),
     path('signup/',views.signup, name='signup' ),
     path('signout/', auth_views.LogoutView.as_view(template_name='user_auth/signout.html'), name='signout'),
-    path('signin/', auth_views.LoginView.as_view(template_name='user_auth/signin.html'), name='signin'),
-
 ]
