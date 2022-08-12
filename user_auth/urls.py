29dc7aeca2e3cdc4bf2a6560ba0d0bef7efb72bf
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-08-12 18:54:55 +0100

    Download button now downloads the library

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -10,5 +10,5 @@ app_name = 'user_auth'
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='user_auth/signin.html'), name='signin'),
     path('signup/',views.signup, name='signup' ),
-    path('signout/', auth_views.LogoutView.as_view(template_name='user_auth/signout.html'), name='signout'),
+    path('signout/', auth_views.LogoutView.as_view(template_name='home/index.html'), name='signout'),
 ]
