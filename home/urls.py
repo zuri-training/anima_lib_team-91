42994ca4c30e06a1ad49ad544b00eb77f2a4e55d
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-08-17 18:52:30 +0100

    Made the authentication pages responsive on mobile

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,9 @@ from . import views
 app_name = 'home' 
 
 urlpatterns = [
-    path('', views.home, name='home'),                       # the website's homepage
-    path('faq', views.faq, name='faq'),                      # the FAQ page
+    path('', views.home, name='home'),                      # the website's homepage
+    path('faq/', views.faq, name='faq'),                    # the FAQ page
+    path('team/', views.team, name='team'),                 # the team page
+    path('terms-and-conditions/', views.terms_and_conditions, name='t_n_c'),        # the terms and conditions page
+    path('privacy-policy', views.privacy_policy, name='privacy_policy'),            # the privacy policy page
 ]
--- home/views.py
+++ home/views.py
@@ -11,3 +11,18 @@ def home(request):
 def faq(request):
     # this view renders the faq.html file to the user
     return render(request, 'home/faq.html', {'title': "FAQs"})
+
+
+def terms_and_conditions(request):
+    # this view renders the Terms and Conditions page
+    return render(request, 'home/t_n_c.html', {'title': "Terms and Conditions"})
+
+
+def privacy_policy(request):
+    # this view renders the Privacy Policy page
+    return render(request, 'home/privacy_policy.html', {'title': "Privacy Policy"})
+
+
+def team(request):
+    # this view renders the Team page
+    return render(request, 'home/team.html', {'title': "The Team"})
